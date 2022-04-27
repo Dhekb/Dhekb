5eea15d2a80f9fde90bb86937bd29fa93d8b7991
aleks <misash@protonmail.com>
2022-04-27 20:12:20 +0300

    Первое сохранение. Init.

---
+++ Packege_Python_for_GIT/__init__.py
@@ -0,0 +1,17 @@
+#КАК ХРАНИТЬ КЛЮЧИ И ПАРОЛИ
+#https://www.youtube.com/watch?v=XvLb-pRuyoU
+
+###########################################################################
+# import os
+#
+# with open('.env','r') as file:
+#     line = file.readline()
+#     os.environ[line[:line.find('=')]] = line[line.find('=') + 1:]
+# API_KEY = os.environ['APY_KEY']
+
+###########################################################################
+import os
+import dotenv
+dotenv.load_dotenv('.env')
+API_KEY = os.environ['APY_KEY']
+print(API_KEY)
